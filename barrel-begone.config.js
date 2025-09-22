93b0d7713728efa02ed63d29482a3175c02893ac
droplister <dan@droplister.com>
2025-09-21 23:35:54 -0400

    chore: remove barrel-begone tool, keep ESLint rule

--- barrel-begone.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  cwd: process.cwd(),
-  maxModuleGraphSize: 50, // Allow up to 50 modules in the graph
-  amountOfExportsToConsiderModuleAsBarrel: 3, // Consider a file a barrel if it has 3+ exports
-  info: true // Enable extra logging
-};
