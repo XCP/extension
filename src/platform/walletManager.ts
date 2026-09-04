e9bbe348c5743a9e3686f0d3ffb408762d62adb6
Dan Anderson <anderson@familymedia.org>
2026-09-03 22:43:46 -0400

    Default new mnemonic wallets to Native SegWit

--- src/platform/walletManager.ts
+++ src/platform/walletManager.ts
@@ -283,7 +283,7 @@ export class WalletManager {
     mnemonic: string,
     password: string,
     name?: string,
-    addressFormat: AddressFormat = AddressFormat.P2TR
+    addressFormat: AddressFormat = AddressFormat.P2WPKH
   ): Promise<Wallet> {
     if (this.wallets.length >= MAX_WALLETS) {
       throw new Error(`Maximum number of wallets (${MAX_WALLETS}) reached`);
